6bfed30a82ceca8dd0c9d809478412e4f8350126
Jin <Jin@nihaoa.com>
2025-10-29 11:53:24 +0800

    实现了Char支持

---
+++ mini-object/mini-object/char.m
@@ -0,0 +1,20 @@
+main()
+{
+	char ch, next;
+    char a,b,c,d;
+    d='d';
+    output d;
+    c='c';
+    output c;
+    b='b';
+    output b;
+    a='a';
+    output a;
+    output "\n";
+	ch = 'X';
+	next = ch + 1;
+	output ch;
+	output "\n";
+	output next;
+	output "\n";
+}
---
+++ mini-object/mini-object/opcode.h
@@ -0,0 +1,7 @@
+#ifndef OPCODE_H
+#define OPCODE_H
+
+#include "inst.h"
+
+#endif /* OPCODE_H */
+
